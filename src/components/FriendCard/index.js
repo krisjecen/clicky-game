a48b833c153fad1b4e695dfda605945a1199cddf
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-07-15 17:52:13 -0700

    changed clickable area to entire image container, eliminated the red x from friendcards

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,26 +4,10 @@ import './style.css'
 function FriendCard (props) {
   return (
     <div className='card'>
-      <div className='img-container'>
+      <div onClick={() => props.removeFriend(props.id)} className='img-container'>
         <img alt={props.name} src={props.image} />
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className='remove'>𝘅</span>
     </div>
   )
 }
-
-// <div className='content'>
-//   <ul>
-//     <li>
-//       <strong>Name:</strong> {props.name}
-//     </li>
-//     <li>
-//       <strong>Occupation:</strong> {props.occupation}
-//     </li>
-//     <li>
-//       <strong>Address:</strong> {props.location}
-//     </li>
-//   </ul>
-// </div>
-
 export default FriendCard
