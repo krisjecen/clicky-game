921ac907dabc9cd5a18b96b5fff37b4656ae3c34
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-07-16 11:39:35 -0700

    game displays instructions and a game status message, notifies the user when they have lost; added comments describing code choices

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import FriendCard from './components/FriendCard'
 import Wrapper from './components/Wrapper'
 import friends from './friends.json'
 import './App.css'
+/* i tried to make the code drier by updating the clicked state array using setState
+but i encountered an error when i used the .push method directly saying that 
+this.state.clicked.push(id) was not a function, so i made another array i could push to.
+*/
 var clickArray = []
 
 class App extends React.Component {
@@ -10,7 +14,8 @@ class App extends React.Component {
     friends,
     clicked: [],
     score: 0,
-    topScore: 0
+    topScore: 0,
+    gameStatus: 'Click any picture below to begin!'
   }  
 
   userPlay = id => {
@@ -20,27 +25,40 @@ class App extends React.Component {
       // ...add the id of the play to the array
       clickArray.push(id)
       // ...increment both scores by one
-      this.setState( { clicked: clickArray, score: this.state.score + 1, topScore: this.state.topScore + 1 })
-    } // 
+      this.setState( { 
+        clicked: clickArray, 
+        score: this.state.score + 1, 
+        topScore: this.state.topScore + 1,
+        gameStatus: 'Good job, keep going!'
+       })
+    } // if the current score is less than the top score
       else if (!clickArray.includes(id) && this.state.score < this.state.topScore) {
+        /* do everything above but don't increment the top score.
+        i tried to make this code drier by setting the topScore equal to the score, but it wouldn't
+        update, it would stay one behind the current score.
+        */
         clickArray.push(id)
-        this.setState( { clicked: clickArray, score: this.state.score + 1 }) 
+        this.setState( { 
+          clicked: clickArray, 
+          score: this.state.score + 1,
+          gameStatus: 'Good job, keep going!'
+        }) 
     } else {
+      /* if the id is already in the array, the player loses and gets a loss message, the score resets,
+      and the clicked array gets emptied
+      */
       clickArray = []
-      this.setState( { clicked: clickArray })
-      this.setState( { score: 0 })
-      console.log("You already clicked that one.")
+      this.setState( { clicked: clickArray, score: 0, gameStatus: 'Oops, you lost. Click a picture to start playing again!' })
     }
 
-    console.log(this.state.clicked)
   }
 
   render() {
     return (
       <Wrapper>
         <h1 className='title'>Clicky Game</h1>
-        <h3 className='title'>Your score: {this.state.score}</h3>
-        <h3 className='title'>Top score: {this.state.topScore}</h3>
+        <h2 className='title'>{this.state.gameStatus} | Don't click the same picture twice!</h2>
+        <h2 className='title'>Your score: {this.state.score} | Top score: {this.state.topScore}</h2>
         {this.state.friends.map(friend => (
           <FriendCard
             userPlay={this.userPlay}
