6dd90b1bd38cf665879267c086551c6c8a1930cf
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-07-15 17:33:02 -0700

    Updates

---
+++ precache-manifest.ff6eb70935f73986bf1c08fd51c8e32c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b958baa58279d4073d67816e05f337",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b3622bff1556191c3529",
+    "url": "/clicky-game/static/css/main.f2c20ad1.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b3622bff1556191c3529",
+    "url": "/clicky-game/static/js/main.9f954d74.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
