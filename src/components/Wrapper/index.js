b22142c4f42edbd36bf50b5bffb70235f15883f8
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-07-19 23:59:32 -0700

    updated styling for greater contrast and smaller picture size

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./style.css";
+import React from 'react'
+import './style.css'
 
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+function Wrapper (props) {
+  return <div className='wrapper'>{props.children}</div>
 }
 
-export default Wrapper;
+export default Wrapper
